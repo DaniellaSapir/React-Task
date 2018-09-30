29d70aa04f49f668dbf63bc504f0947ce732ac59
Daniella Sapir <daniella.sapir@gmail.com>
2018-09-30 22:07:58 +0300

    Updated Task

--- src/Task/task.js
+++ src/Task/task.js
@@ -2,57 +2,35 @@ import React from 'react';
 
 export default class ToggleDiv extends React.Component {
 
-    constructor(props) {
-        super(props);
-        //DO NOT directly set state, unless in the Constructor
+    constructor() {
+        super();
+        //STATE is a class property used ONLY with Components not on Functions. Props are set and passed from outside the comp(attr into Task comp).State is managed from inside the comp and changes app state. DO NOT directly set state, unless in the Constructor
         this.state = {className: 'down'};
          // This binding is necessary to make `this` work in the callback
         this.changeState = this.changeState.bind(this);
     }
 
-    //one way - use timeout to return the div className back to down
+    //define class toggle method
     changeState(e) {
-        // ADD EVENT LISTENER TO PREVENT DEFAULT
+        // PREVENT outer div EVENT propagation 
         e.stopPropagation();
-        // e.preventDefault();
-        //we can catch values of target el. on which the event is working (returns down)
-        const state = e.target.className;
-        console.log(state);
         
+        //catch values of target el. on which the event is working (returns div current state)
+        const state = e.target.className;
+        // console.log(state);
+
+        //TOGGLE STATE based on current state value
         if((state === 'down')||(state === 'flyDown')) {
             this.setState({className: 'flyUp'});
         }
         else {
             this.setState({className: 'flyDown'});
         }
-
-        // setTimeout(() => {
-        //     this.setState({className: 'down'});
-        // }, 1000);
     }
 
     render() {
         return (
-            <div className={this.state.className} onClick={this.changeState}> Down
-                {/* {props.children} */}
-            </div>
+            <div className={this.state.className} onClick={this.changeState}> Down </div>
         );
     }
   }
-//a component is basically a func. returning JSX -- BEST PRACTICE TO USE! don't alter the state
-// const taskDiv = (props) => {
-    //create outer div
-    // return (
-        //catch a reference to handler
-        // <div className="inner" onClick={props.onClick}>
-    // {/* Generate dynamic data */}
-            // {/* <p>I am about {Math.floor(Math.random() * 30)} years old! :)</p> */}
-    // {/*pass data using obj props(properties which are comp.attributes).To use pass it as arg. in definition*/}
-            // {/* <p>I am {props.name} about {props.age} years old! :)</p> */}
-    // {/* we can use props.children in order to add any child element */}
-            // {props.children}
-        // </div>
-    // );
-// }
-
-
--- src/Task2/task2.js
+++ src/Task2/task2.js
@@ -1,14 +1,16 @@
 // import React from 'react';
 //USE FUNCTIONAL COMPONENTS, they DON'T HAVE THEIR OWN THIS (not for obj. methods)
 
-const genNumbers= (props) => {
+const GenNumbers= (props) => { 
     var originalArray =[];
     var temp48 = [];
 
     originalArray = Array.from({length: 10}, (v, i) => i); // generate sequenced array 
     // OR:  const range = (start, stop, step) => Array.from({ length: (stop - start) / step }, (_, i) => start + (i * step));
     // var originalArray = Object.keys(range(start, stop, step));
+    console.log('Original Array is :'); 
     console.log(originalArray); 
+
 
     // 1 QUESTION  ------ Get all the numbers that are bigger than 4 and smaller than 8 in the originalArray - print them in the console
 
@@ -20,7 +22,9 @@ const genNumbers= (props) => {
             temp48.push(originalArray[i]);
         } 
     }
+    console.log('First question: numbers that are bigger than 4 and smaller than 8 ');
     console.log(temp48);
+
 
     // 2 QUESTION  ------- Multiple all the numbers that are bigger than 4 and smaller than 8 in the originalArray, by 10 - print them in the console
 
@@ -28,23 +32,28 @@ const genNumbers= (props) => {
     var multi10 = temp48.map(function(num) {
         return num * 10;
     });
+    console.log('Second question: multiply the result from previous by 10');
     console.log(multi10);
+
 
     // 3 QUESTION  ------- Calculate the sum of the numbers that are bigger than 4 and smaller than 8, multiplied by 10 - print it in the consol
 
     //multiply the SUM of prev. result by 10, use Array.prototype.reduce() 
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
     // 5+6+7
+    console.log('Third question: multiply the SUM of prev. result by 10');
     console.log((temp48.reduce(reducer))*10);
     // expected output: 180
 
     //should be empty I added onClick for fun
     return (
         null
-        // <div onClick={genNumbers}> Array of numbers : 
-        //     // return React.createElement('div', null, React.createElement('h1', {className: 'App'} ,'Hi this is Daniella'));  //h1 rendered with the styles for class App
-        //     <div>{props.children}</div>
+        // <div onClick={genNumbers}> 
+        //  React.createElement('div', null, React.createElement('h1' ,'Hi this is Daniella'))); 
+        //     {/* // <div>{props.children}</div> */}
         // </div>
     );
 }
-export default genNumbers;
+export default GenNumbers;
+
+
